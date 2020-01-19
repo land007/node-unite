5bb521b6be254109ececd2b13d185b1abee233fe
阿七 <yiqiujia@hotmail.com>
2020-01-19 09:11:57 +0800

    test

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# node-whfmthify
+# node-unite
 Wait here for me to handle it for you
+
 在这等着我给你们统一办理
+
+```JavaScript
+const whfmthify = require('whfmthify');
+
+var crawling = function(from, till, max) {
+	return new Promise(async function(resolve, reject) {
+	});
+};
+var reptile = function(from, till, max, force) {
+	return new Promise(async function(resolve, reject) {
+		let items = await whfmthify(crawling, from, till, max);
+		resolve(items);
+	});
+};
+
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+const Signal = require('await-signal');
+const { promisify } = require('util');
+const sleep = promisify(setTimeout);
+
+var signals = {};
+
+let unite = async function() {
+//	console.log('arguments', arguments);
+	_arguments = [];
+	let i = 0;
+	let fun = arguments[0];
+	if(fun === undefined) {
+		return;
+	}
+	if(typeof fun != 'function') {
+		return;
+	}
+	for(let a in arguments) {
+		if(i > 0) {
+			_arguments[_arguments.length] = arguments[a];
+		}
+		i++;
+	}
+	let key = JSON.stringify(_arguments);
+//	console.log('key', key);
+	if(signals[key] === undefined) {
+		signals[key] = {signal: new Signal('default')};
+		let data = await fun.apply(this, _arguments);
+		signals[key].value = data;
+		signals[key].signal.state = 'ok';
+//		console.log('run1', data);
+		await sleep(10);
+		delete signals[key];
+		return data;
+	} else {
+		await signals[key].signal.until('ok');
+		let data = signals[key].value;
+//		console.log('run2', data);
+		return data;
+	}
+};
+
+module.exports = unite;
